45d6716b259311460563c043cd9e754769cc912f
Appo <Appo@MONSTER>
2016-09-29 15:58:58 +0500

    Есть  ввод строки и ввод текста. ввод текста еще не тестил.

--- CPP_Home_04/CPP_Home_04.cpp
+++ CPP_Home_04/CPP_Home_04.cpp
@@ -2,10 +2,48 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <ctype.h>
 
+using namespace std;
+
+
+
+char* gets()
+{
+	char c, *str = 0;
+	int cnt = 1;
+	while ((c = getchar()) != '\n')
+	{
+		str = (char*)realloc(str, ++cnt);
+		str[cnt-2] = c;
+	}
+	if (cnt != 1)  str[cnt - 1] = 0;
+	return str;
+}
+
+char** TextIn()
+{
+	char *s = 0, **text = 0;
+	cout << "¬ведите текст" << endl;
+	int cnt = 1, size = 1;
+	while ((s = gets()) != 0)
+	{
+		size += strlen(s);
+		text = (char**)realloc(text, size);
+		text[++cnt - 2] = s;
+	}
+	if (*text) text[cnt - 1] = 0;
+	return text;
+}
 
 int main()
 {
+	setlocale(0,"");
+	
+	
+	
+	getchar();
     return 0;
 }
 
